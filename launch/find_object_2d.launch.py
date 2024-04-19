95d5e80ded5cb1197bd7a764bbb3e5b5dcc705be
s3835358 <s3835358@student.rmit.edu.au>
2024-04-19 16:56:35 +1000

    Include objects in repo

--- launch/find_object_2d.launch.py
+++ launch/find_object_2d.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
         # Uses the path to the AIIL Workspace, but this could be set to anywhere
         LogInfo(msg=('AIIL_CHECKOUT_DIR, ', EnvironmentVariable(name='AIIL_CHECKOUT_DIR'))),
         DeclareLaunchArgument('objects_path', 
-                               default_value=[EnvironmentVariable(name='AIIL_CHECKOUT_DIR'),'/humble_workspace/objects'],
+                               default_value=[EnvironmentVariable(name='AIIL_CHECKOUT_DIR'),'/humble_workspace/src/snchallenge/objects'],
                                description='Directory containing objects to load on initialization.'),
         
         # Find Object 2D Setting. By default just use the standard settings, but you can copy and tweak this file if you wish
