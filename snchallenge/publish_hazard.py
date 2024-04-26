2f69620884e910c1cf2e48a01151ae64c5514259
s3835358 <s3835358@student.rmit.edu.au>
2024-04-26 07:09:38 +0000

    Measure hazard position

--- snchallenge/publish_hazard.py
+++ snchallenge/publish_hazard.py
@@ -2,13 +2,24 @@
 
 import rclpy
 from rclpy.node import Node
+
+from rclpy.qos import ReliabilityPolicy, QoSProfile
+
 from std_msgs.msg import Float32MultiArray
 
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+import tf2_geometry_msgs
+from geometry_msgs.msg import PoseStamped
+
+from collections import OrderedDict
 
 import math
 import numpy as np
 import cv2 as cv
+import tf2_ros
 
 class HazardPublisher(Node):
     def __init__(self):
@@ -41,6 +52,35 @@ class HazardPublisher(Node):
                             10
                     )
 
+        # Subscriber for /Objects
+        self.sub_be = self.create_subscription(
+                            LaserScan,
+                            '/scan',
+                            self.scan_listener,
+                            QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE)
+                    )
+        
+        self.scan_dict = OrderedDict()
+        
+        # Transform listener
+        self.tf_buffer = tf2_ros.buffer.Buffer()
+        self.tf_listener = tf2_ros.transform_listener.TransformListener(self.tf_buffer, self)
+
+    def scan_listener(self, msg):
+        timeStamp = msg.header.stamp
+        keyTime = round(timeStamp.sec + timeStamp.nanosec/1000000000, 1) # convert nano to seconds, 
+        # publisher already rounds to single decimal place here we explicitely enforce it
+
+        self.scan_dict[keyTime] = msg.ranges
+        #self.get_logger().info(f"Laser time: {timeStamp}")
+        
+        #self.get_logger().info(f"Key time: {keyTime}")
+        
+
+        if len(self.scan_dict) > 30:
+            item = self.scan_dict.popitem(last=False) # pop oldest entry
+            #self.get_logger().info(f"Range: {self.scan_dict[keyTime][0]}")
+
     def publish_hazard(self, details):
         # Current time 
         time = self.get_clock().now()
@@ -54,33 +94,42 @@ class HazardPublisher(Node):
         marker_msg.action = Marker.ADD
         
         # Marker ID
-        marker_msg.id = details[0]
+        marker_msg.id = int(details[0])
 
-        marker_msg.pose.position.x = 1.0 * self.xmul
-        marker_msg.pose.position.y = 2.0
-        marker_msg.pose.position.z = 0.0
-        marker_msg.pose.orientation.x = 0.0
-        marker_msg.pose.orientation.y = 0.0
-        marker_msg.pose.orientation.z = 0.0
-        marker_msg.pose.orientation.w = 1.0
-        
-        marker_msg.scale.x = 1.0 
-        marker_msg.scale.y = 1.0
-        marker_msg.scale.z = 1.0
-        marker_msg.color.r = 1.0
-        marker_msg.color.g = 0.0
-        marker_msg.color.b = 0.0
-        marker_msg.color.a = 1.0
-        
-        # Infinite lifetime
-        marker_msg.lifetime.sec = 0
+        #todo CAMERA HEIGHT
 
-        # Publish
-        self.pub.publish(marker_msg)
+        self.get_logger().info(f"Trasnform1") 
+        poseT = self.transform([details[1], details[2], 0.0], 'camera_color_frame', 'map')
+        
+        if(poseT):
+            self.get_logger().info(f"PoseT: {poseT.pose}")
+            
+            marker_msg.pose.position.x = poseT.pose.position.x
+            marker_msg.pose.position.y = poseT.pose.position.y
+            marker_msg.pose.position.z = poseT.pose.position.z
+
+            marker_msg.pose.orientation.x = 1.0
+            marker_msg.pose.orientation.y = 0.0
+            marker_msg.pose.orientation.z = 0.0
+            marker_msg.pose.orientation.w = 0.0
+            
+            marker_msg.scale.x = 1.0 
+            marker_msg.scale.y = 1.0
+            marker_msg.scale.z = 1.0
+            marker_msg.color.r = 1.0
+            marker_msg.color.g = 0.0
+            marker_msg.color.b = 0.0
+            marker_msg.color.a = 1.0
+            
+            # Infinite lifetime
+            marker_msg.lifetime.sec = 0
+
+            # Publish
+            self.pub.publish(marker_msg)
         #self.get_logger().info('Visualization marker published.')
         
         self.xmul = -self.xmul
-    
+
     def object_listener(self, msg):
         #MultiArrayLayout  layout        # specification of data layout
             #MultiArrayDimension[] dim #
@@ -91,44 +140,132 @@ class HazardPublisher(Node):
         #float32[]         data          # array of data
         if(msg.data):
             self.get_logger().info(f'Object Data: {msg.data[0]}')
-            self.get_object_position(msg.data)
+            time = self.get_clock().now()
+            #time = rclpy.time.Time()
+            
+            self.get_object_position(msg.data, time)
     
     # Code based on https://husarion.com/tutorials/ros-tutorials/5-visual-object-recognition/#recognizing-objects
-    def get_object_position(self, data):
-        CAMERA_WIDTH = 640 # left 0, right 640
+    def get_object_position(self, data, time):
+        
+        id = data[0]
+        object_width = data[1]
+        object_height = data[2]
 
-        MIN_ANG_VEL = 0.15
-        MAX_ANG_VEL = 0.5
-        ANGULAR_GAIN = 2e-3 
-        #int obj_x_pos;
-        #float ang_vel;
+        cv_homography = np.zeros((3, 3), dtype=np.float32)
 
-        # Reset linear and angular speed value
-        #vel_msg.linear.x = 0;
-        #vel_msg.angular.z = 0;
+        for i in range(9):
+            cv_homography[i % 3, i // 3] = data[i + 3]
 
-        if data:
-            id = data[0]
-            objectWidth = data[1]
-            objectHeight = data[2]
+        in_pts = np.array([[0, 0], [object_width, 0], [0, object_height], [object_width, object_height]], dtype=np.float32)
+        in_pts = np.expand_dims(in_pts, axis=1)
+        
+        out_pts = cv.perspectiveTransform(in_pts, cv_homography)
 
-            inPts = np.array([[0, 0], [objectWidth, 0], [0, objectHeight], [objectWidth, objectHeight]], dtype=np.float32)
-            hMatrix = np.array([[data[3],data[4],data[5]],[data[6],data[7],data[8]],[data[9],data[10],data[11]]], dtype=np.float32).reshape((3, 3))
-            inPts = np.array([inPts])
+        obj_x_pos = np.mean(out_pts[:, :, 0])
+        
+        self.get_logger().info(f"X Pos: {obj_x_pos}")
 
-            dest = cv.perspectiveTransform(inPts, hMatrix)
-            self.get_logger().info(f'Dest: {dest}')
+        #minX = 0
+        #maxX = 320
+
+        factor = 160 - obj_x_pos
+
+        #minAng = 315
+        #maxAng = 45
+
+        laser = 315 + round(obj_x_pos/320*90)
+
+        if(laser > 359):
+            laser = laser - 360
+
+        depth = self.get_depth(time, laser) 
+
+        xValue = depth - abs(factor/320)
+        yValue = depth*(factor/320)
+
+        self.get_logger().info(f"laser: {laser}")
+        self.get_logger().info(f"xValue: {xValue}")
+        self.get_logger().info(f"yValue: {yValue}")
+        
+        details = [id, xValue, yValue]
+        
+        self.publish_hazard(details)
+
+    def get_depth(self, timestamp, range):
+
+        self.get_logger().info(f"Time: {timestamp}")      
+        timestamp = 0
+  
+
+        depth = 0.5
+
+        if timestamp in self.scan_dict:
+            depth = self.scan_dict[timestamp][range]
+            self.get_logger().info(f"Found depth: {depth}")
+        
+        elif self.scan_dict:
+            self.get_logger().info(f"Using range: {range}")
+            depth = self.scan_dict.popitem(last=True)[range] # most recent entry
+
+        return depth
+        
+
+    def transform(self, srcPose, src, dest):
+        
+        poseT = None
+
+
+        try:
+            # Current time CONFIGURABLE 
+            #time = self.get_clock().now() - rclpy.duration.Duration(seconds=0.4)
+            time = rclpy.time.Time()
+            #self.get_logger().info(str(time))
+            self.get_logger().info(f"Transform2") 
+
+            pose = PoseStamped()
+            pose.header.frame_id = src
+            pose.header.stamp = time.to_msg()
             
-            obj_x_pos = (dest[0][0][0] + dest[0][1][0] + dest[0][2][0] + dest[0][3][0]) / 4
-            ang_vel = ANGULAR_GAIN*(CAMERA_WIDTH/2 - obj_x_pos)
+            pose.pose.position.x = srcPose[0]
+            pose.pose.position.y = srcPose[1]
+            pose.pose.position.z = srcPose[2]
+            
+            # Equivalent to 0,0,0 roll, pitch, yaw
+            pose.pose.orientation.x = 1.0
+            pose.pose.orientation.y = 0.0
+            pose.pose.orientation.z = 0.0
+            pose.pose.orientation.w = 0.0
 
-            self.get_logger().info(f'XPos: {obj_x_pos}')
-            self.get_logger().info(f'Angle: {ang_vel}')            
+            # Configure frames
+            
+            #self.get_logger().info(f"From frame: {src}")
+            #self.get_logger().info(f"To frame: {dest}")
 
+            # Timeout for transform data
+            timeout = rclpy.duration.Duration(seconds=0.8)
+
+            # Lookup a transform
+            #transform = self.tf_buffer.lookup_transform(dest, src, time, timeout=timeout)
+            #self.get_logger().info(f"Transform: {transform.transform}")
+            self.get_logger().info(f"Tranform3")
+
+            poseT = self.tf_buffer.transform(pose, dest)
+            
+            self.get_logger().info(f"Transform4")
+
+        except tf2_ros.ExtrapolationException as ex:
+            #pass
+            self.get_logger().info(f'Could not gain current data for {src} to {dest}: {ex}')
+        except tf2_ros.TransformException as ex2:
+            #pass
+            self.get_logger().info(f'Could not transform {src} to {dest}: {ex2}')
+        
+        return poseT
 def main():
     rclpy.init()
     node = HazardPublisher()
-    
+
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
